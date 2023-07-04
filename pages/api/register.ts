0a05f2576fe82636bd9ec8c0de42f65f8a5ff048
nrenner <nrenner0211@gmail.com>
2023-07-04 15:01:23 -0400

    register functionality in progress

---
+++ pages/api/register.ts
@@ -0,0 +1,17 @@
+import bcrypt from 'bcrypt';
+import { NextApiRequest, NextApiResponse } from 'next';
+import prismadb from '@/lib/prismadb';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        return res.status(405).end();
+    }
+
+    try {
+        const { email, name, password } = req.body;
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).end();
+    }
+}
